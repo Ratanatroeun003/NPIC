9e55170189ede805bbbec071ad2c4e627fe6372d
Ratanatroeun003 <rnatroeun@gmail.com>
2026-01-05 15:25:01 +0700

    update

--- index.php
+++ index.php
@@ -2,8 +2,16 @@
 <?php
 include 'includes/header.inc.php';
 include 'includes/navbar.inc.php';
+$available_page = ['login', 'register'];
+
 if (isset($_GET['page'])) {
     $page = $_GET['page'];
-    include 'pages/' . $page . '.php';
+    if (in_array($page, $available_page)) {
+        include 'pages/' . $page . '.php';
+    } else {
+        echo '<h1>Page not found(404)</h1/';
+    }
+} else {
+    echo '<h1>Page not found(404)</h1/';
 }
 include 'includes/footer.inc.php';
